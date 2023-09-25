2480ba3453c094fe1c1a7f12d053833c8a8bc56b
mohamedmehdigara <garamehdi57@yahoo.com>
2023-09-25 17:56:42 +0100

    added files

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -7,37 +7,17 @@ const AsteroidWrapper = styled.div`
   height: 40px;
   background-color: #ff0000;
 `;
-const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 const Asteroid = ({ top, left }) => {
-  const width = 20; // Set the default width
-  const height = 20; // Set the default height
-
-  const getBoundingBox = () => {
-    return {
-      x: left,
-      y: top,
-      width: width,
-      height: height,
-    };
-  };
+  const width = 40;
+  const height = 40;
 
   return (
-    <AsteroidWrapper style={{ top, left, backgroundColor: randomColor, width, height }} />
+    <AsteroidWrapper
+      style={{ top, left, width, height }}
+      data-bounding-box={`{ "x": ${left}, "y": ${top}, "width": ${width}, "height": ${height} }`}
+    />
   );
-};
-
-export const getBoundingBox = (top, left) => {
-  const width = 20; // Set the default width
-  const height = 20; // Set the default height
-
-  return {
-    x: left,
-    y: top,
-    width: width,
-    height: height,
-  };
 };
 
 export default React.memo(Asteroid);
