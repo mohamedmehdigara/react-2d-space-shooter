ea49f23ad0c1ca3757833516f52c9d501e3c8a66
mohamedmehdigara <garamehdi57@yahoo.com>
2023-09-26 16:36:10 +0100

    added files

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -3,15 +3,25 @@ import styled from 'styled-components';
 
 const AsteroidWrapper = styled.div`
   position: absolute;
-  width: 40px;
-  height: 40px;
-  background-color: #ff0000;
+  background-color: ${(props) => props.color || '#ff0000'};
+  animation: rotate 5s linear infinite; /* Add rotation animation */
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
-const Asteroid = ({ top, left }) => {
-  const width = 40; // Set the default width
-  const height = 40; // Set the default height
-
+const Asteroid = ({ top, left, width, height }) => {
+  const minSize = 20;
+  const maxSize = 60;
+  const randomSize = Math.floor(Math.random() * (maxSize - minSize + 1) + minSize);
+  const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
   const getBoundingBox = () => {
     return {
       x: left,
@@ -22,7 +32,15 @@ const Asteroid = ({ top, left }) => {
   };
 
   return (
-    <AsteroidWrapper style={{ top, left, width, height }} />
+    <AsteroidWrapper
+      style={{
+        top,
+        left,
+        width: randomSize,
+        height: randomSize,
+      }}
+      color={randomColor}
+    />
   );
 };
 
